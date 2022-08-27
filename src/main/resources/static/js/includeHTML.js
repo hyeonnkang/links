4ad650c724205ed8ff962af8156736036120b1df
kang hyeon <hyeonkang.dev@gmail.com>
2022-08-27 22:57:02 +0900

    html 파일 분리하기

---
+++ src/main/resources/static/js/includeHTML.js
@@ -0,0 +1,15 @@
+function nav(){
+    document.write(`
+    <p>
+    <a href="/boards">글목록</a>
+    <a href="/boards/create">글쓰기</a>
+    <a href="/members/login">로그인</a>
+    <a href="/members/signup">회원가입</a>
+    </p>
+    `);
+}
+
+function header(){
+    document.write(`<h1><a href="/">링크스</a></h1>
+    `);
+}
